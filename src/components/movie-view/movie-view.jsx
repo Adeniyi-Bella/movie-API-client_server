76c9f37edc2d5e6d0faeeedda86a63892369262d
Adeniyi Bella <adeniyibella@gmail.com>
2022-11-08 22:01:37 +0100

    movie view was edited to reflect naming of properties in remote DB

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -4,11 +4,11 @@ export class MovieView extends React.Component {
 
   render() {
     const { movie, onBackClick } = this.props;
-
+console.log(typeof(movie.image));
     return (
       <div className="movie-view">
         <div className="movie-poster">
-          <img src={movie.ImagePath} />
+          <img src={movie.image} />
         </div>
         <div className="movie-title">
           <span className="label">Title: </span>
@@ -16,11 +16,17 @@ export class MovieView extends React.Component {
         </div>
         <div className="movie-description">
           <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
+          <span className="value">{movie.description}</span>
         </div>
         <button onClick={() => { onBackClick(null); }}>Back</button>
 
       </div>
     );
   }
+
+  // componentDidMount() {
+  //   document.addEventListener('keypress', event => {
+  //     console.log(event.key);
+  //   });
+  // }
 }
