84de854553f05485d3c58a572e3b6568f3b2ef6b
Adeniyi Bella <adeniyibella@gmail.com>
2022-11-08 22:00:36 +0100

    prop types was added for datatype checking

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick } = this.props;
-
+    console.log(movie.image);
     return (
       <div
         className="movie-card"
@@ -16,3 +17,13 @@ export class MovieCard extends React.Component {
     );
   }
 }
+
+MovieCard.propTypes = {
+  
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
