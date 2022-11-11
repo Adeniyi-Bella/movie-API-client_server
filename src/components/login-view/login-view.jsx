2ee142e054aabd23b9d0c737cd8ebb7824792ebc
Adeniyi Bella <adeniyibella@gmail.com>
2022-11-11 18:43:14 +0100

    added navbar to login page with bootstrap styling

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,11 @@
 import React, { useState } from 'react';
+import {
+  Nav,
+  Navbar,
+  NavDropdown,
+
+} from 'react-bootstrap';
+// import Button from 'react-bootstrap/Button';
 import './login-view.scss';
 export function LoginView(props) {
   const [username, setUsername] = useState('');
@@ -13,56 +20,85 @@ export function LoginView(props) {
   };
 
   return (
+    <>
+    <Navbar bg="light" variant="light" expand="lg" sticky="top">
+        {/* <Container> */}
+        <Navbar.Brand href="#home">My-Movie APP</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">
+                Action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">
+                Something
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+        {/* </Container> */}
+      </Navbar>
+
     <div className="limiter">
       <div className="container-login100">
         <div className="wrap-login100">
           <form className="login100-form validate-form">
             <span className="login100-form-title p-b-26">Welcome</span>
-            <span class="login100-form-title p-b-48">
-              <i class="zmdi zmdi-font"></i>
+            <span className="login100-form-title p-b-48">
+              <i className="zmdi zmdi-font"></i>
             </span>
 
             <div
-              class="wrap-input100 validate-input"
+              className="wrap-input100 validate-input"
               data-validate="Valid email is: a@b.c"
             >
               <input
-                class="input100"
+                className="input100"
                 type="text"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
               />
               <span
-                class="focus-input100"
+                className="focus-input100"
                 data-placeholder="Email"
               ></span>
             </div>
 
             <div
-              class="wrap-input100 validate-input"
+              className="wrap-input100 validate-input"
               data-validate="Enter password"
             >
-              <span class="btn-show-pass">
-                <i class="zmdi zmdi-eye"></i>
+              <span className="btn-show-pass">
+                <i className="zmdi zmdi-eye"></i>
               </span>
               <input
-                class="input100"
+                className="input100"
                 type="password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
 
               <span
-                class="focus-input100"
+                className="focus-input100"
                 data-placeholder="Password"
               ></span>
             </div>
 
-            <div class="container-login100-form-btn">
-              <div class="wrap-login100-form-btn">
-                <div class="login100-form-bgbtn"></div>
+            <div className="container-login100-form-btn">
+              <div className="wrap-login100-form-btn">
+                <div className="login100-form-bgbtn"></div>
                 <button
-                  class="login100-form-btn"
+                  className="login100-form-btn"
                   type="submit"
                   onClick={handleSubmit}
                 >
@@ -71,10 +107,10 @@ export function LoginView(props) {
               </div>
             </div>
 
-            <div class="text-center p-t-115">
-              <span class="txt1">Don’t have an account?</span>
+            <div className="text-center p-t-115">
+              <span className="txt1">Don’t have an account?</span>
 
-              <a class="txt2" href="#">
+              <a className="txt2" href="#">
                 Sign Up
               </a>
             </div>
@@ -82,5 +118,6 @@ export function LoginView(props) {
         </div>
       </div>
     </div>
+    </>
   );
 }
