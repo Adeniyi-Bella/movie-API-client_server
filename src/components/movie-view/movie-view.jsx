2066670a04e4d425b260276260fb0087b34f9292
Adeniyi Bella <adeniyibella@gmail.com>
2022-11-16 00:28:20 +0100

    movie view was updated to include names of actors, genre

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom'
 
 export class MovieView extends React.Component {
+
   render() {
-    const { movie, onBackClick } = this.props;
+    const { movie, onBackClick, addFavorite } = this.props;
     console.log(movie);
+    const name = []
+    movie.actors.forEach(element => {
+      name.push(element.name)
+    });
+    // console.log(name);
     return (
       <Card border="light" body style={{}}>
         <Card.Body>
@@ -15,14 +22,47 @@ export class MovieView extends React.Component {
             src={movie.image}
             crossOrigin="anonymous"
           />
-          <Card.Title>
+          <Card.Title style={{ padding:  '10px 0'}}>
             <span className="label">Title: </span>
             <span className="value">{movie.Title}</span>
           </Card.Title>
           <Card.Text>
-            <span className="label">Description: </span>
-            <span className="value">{movie.description}</span>
+            <span className="label">Actor/s: </span>
+            <span className="value">{`${name}`}</span>
           </Card.Text>
+          <Card.Text>
+            <span className="label">Release year/Rating/ Popularity: </span>
+            <span className="value">{`${movie.release_year}/${movie.rating}/${movie.popularity}`}</span>
+          </Card.Text>
+          <Card.Text>
+            <span className="label">Description: </span>
+            <span className="value">{movie.description || null}</span>
+          </Card.Text>
+          <Card.Text>
+            <span className="label">Director/s: </span>
+            <span className="value">{`${movie.director_names}`}</span>
+          </Card.Text>
+          <Card.Text>
+            <span className="label">Genre/s: </span>
+            <span className="value">{`${movie.genres}`}</span>
+          </Card.Text>
+          <Card.Text>
+            <span className="label">Language/s: </span>
+            <span className="value">{`${movie.languages}`}</span>
+          </Card.Text>
+          <Button
+            className="button-movie-view-add-favorite"
+            variant="outline-warning"
+            size="sm"
+            type="button"
+            onClick={() => addFavorite(movie._id)}
+          >
+            Add to favorites
+          </Button>
+          {/*
+          <Link to={`/genres/${movie.Genre.Name}`}>
+            <Button variant="link">Genre</Button>
+          </Link> */}
           <Button
             onClick={() => {
               onBackClick(null);
