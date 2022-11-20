ba99c613dd74088085d6752f1b7308bd48db4b3a
Adeniyi Bella <adeniyibella@gmail.com>
2022-11-20 20:15:49 +0100

    movie list component created to load movie card

---
+++ src/components/movies-list/movies-list.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Row from 'react-bootstrap/Col';
+import { connect } from 'react-redux';
+import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input';
+import { MovieCard } from '../movie-card/movie-card';
+
+const mapStateToProps = (state) => {
+  const { visibilityFilter } = state;
+  return { visibilityFilter };
+};
+
+function MoviesList(props) {
+  const { movies, visibilityFilter } = props;
+  console.log(visibilityFilter);
+  let filteredMovies = movies;
+
+  if (visibilityFilter !== '') {
+    console.log(18);
+    filteredMovies = movies.filter((m) =>
+      m.Title.toLowerCase().includes(visibilityFilter.toLowerCase())
+    );
+  }
+
+  if (!movies) return <div className="main-view" />;
+
+  return (
+    <Row xs={1} md={2}>
+      <VisibilityFilterInput visibilityFilter={visibilityFilter} />
+      {filteredMovies.map((m) => (
+        <MovieCard movie={m} key={m._id}/>
+      ))}
+    </Row>
+  );
+}
+
+export default connect(mapStateToProps)(MoviesList);
