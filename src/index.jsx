562d61500c648d94154eb680fa5b9f17f530bd30
Adeniyi Bella <adeniyibella@gmail.com>
2022-11-23 23:57:10 +0100

    created redux store in inside our app react file

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
-import { MainView } from './components/main-view/main-view';
-import { createRoot } from 'react-dom/client'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+import moviesApp from './reducers/reducers';
+import  MainView  from './components/main-view/main-view';
+import { createRoot } from 'react-dom/client';
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
+const store = createStore(moviesApp, devToolsEnhancer());
+
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
-    render() {
-      return (
-       
+  render() {
+    return (
+      <Provider store={store}>
+        {/* <Container> */}
           <MainView />
-        
-      );
-    }
+        {/* </Container> */}
+      </Provider>
+    );
   }
+}
 
-  // Finds the root of your app
+// Finds the root of your app
 const container = document.getElementsByClassName('app-container')[0];
 console.log(container);
 const root = createRoot(container);
